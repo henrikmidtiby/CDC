2b5eca3710838579ec0ddcf2b7954fe2f0ce1c19
Søren Vad Iversen <sorenvad@hotmail.com>
2024-08-12 08:36:30 +0200

    Changing folder structure

--- transform.py
+++
@@ -1,8 +0,0 @@
-#For adding functionality to make transformation between different colorspaces 
-
-def transform_image(img,transform=None,):
-    if transform== None:
-        return img
-    
-    transformed_img=transform(img)
-    return transformed_img
