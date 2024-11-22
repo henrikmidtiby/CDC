9223564c93e82f58613a1865cca766666f35a61a
Henrik Dyrberg Egemose hde <hde@mmmi.sdu.dk>
2024-11-22 13:07:38 +0100

    formatted and linted

--- ROC/ROC.py
+++ ROC/ROC.py
@@ -1,6 +1,7 @@
-import numpy as np
 import matplotlib.pyplot as plt
 import mplcursors
+import numpy as np
+
 
 class ROC:
     def __init__(self) -> None:
@@ -8,42 +9,40 @@ class ROC:
         self.false_positive = []
         self.true_negative = []
         self.false_negative = []
-        self.thresholds=[]
-        self.true_positives_rate=None
-        self.false_positives_rate=None
-        self.precision=None 
+        self.thresholds = []
+        self.true_positives_rate = None
+        self.false_positives_rate = None
+        self.precision = None
 
-    
-# arguments must be numpy arrays
-    def get_points(self,analysed_image,test_positive,test_negative,number_of_samples,model=None):
-        """ """
+    # arguments must be numpy arrays
+    def get_points(self, analysed_image, test_positive, test_negative, number_of_samples, model=None):
         if model is None:
+
             def model(value):
                 return value
-        
-        analysed_image_temp = np.reshape(analysed_image,-1)
-        test_positive_temp  = np.reshape(test_positive,-1)
-        test_negative_temp  = np.reshape(test_negative,-1)
-        thresholds          = np.linspace(0,255,number_of_samples)
+
+        analysed_image_temp = np.reshape(analysed_image, -1)
+        test_positive_temp = np.reshape(test_positive, -1)
+        test_negative_temp = np.reshape(test_negative, -1)
+        thresholds = np.linspace(0, 255, number_of_samples)
 
         for threshold in thresholds:
-            true_positive       = 0
-            false_positive      = 0
-            true_negative       = 0
-            false_negative      = 0
-        
-            for i, value in np.ndenumerate(analysed_image_temp):
-                if model(value)     <=   threshold:
-                    if test_positive_temp[i]    ==  255:
-                        true_positive   +=  1
-                    if test_negative_temp[i]    ==  255:
-                        false_positive  +=  1
-                else:
-                    if test_negative_temp[i]    ==  255:
-                        true_negative   +=  1
-                    if test_positive_temp[i]    ==  255:
-                        false_negative  +=  1
+            true_positive = 0
+            false_positive = 0
+            true_negative = 0
+            false_negative = 0
 
+            for i, value in np.ndenumerate(analysed_image_temp):
+                if model(value) <= threshold:
+                    if test_positive_temp[i] == 255:
+                        true_positive += 1
+                    if test_negative_temp[i] == 255:
+                        false_positive += 1
+                else:
+                    if test_negative_temp[i] == 255:
+                        true_negative += 1
+                    if test_positive_temp[i] == 255:
+                        false_negative += 1
 
             self.true_positive.append(true_positive)
             self.false_positive.append(false_positive)
@@ -51,118 +50,107 @@ class ROC:
             self.false_negative.append(false_negative)
             self.thresholds.append(threshold)
 
+    def distance_to_points(self, analysed_image, test_positive, test_negative):
+        positive_mask = np.reshape(test_positive, (-1))
+        negative_mask = np.reshape(test_negative, (-1))
 
-    def distance_to_points(self,analysed_image,test_positive,test_negative):
+        distances = np.reshape(analysed_image, (-1))
 
-        positive_mask=np.reshape(test_positive,(-1))
-        negative_mask=np.reshape(test_negative,(-1))
+        positive_distances = np.sort(distances[positive_mask >= 200])
+        negative_distances = np.sort(distances[negative_mask >= 200])
 
-        distances=np.reshape(analysed_image,(-1))
-        
-        positive_distances=np.sort(distances[positive_mask>=200])
-        negative_distances=np.sort(distances[negative_mask>=200])
-        
-        i=0
-        n=0
-        while(i<len(positive_distances)):
-            threshhold=positive_distances[i]
+        i = 0
+        n = 0
+        while i < len(positive_distances):
+            threshold = positive_distances[i]
 
-            if threshhold < negative_distances[n]:
-                true_pos=i+1
-                false_negative=len(positive_distances)-true_pos
-                false_positive=n
-                true_negative=len(negative_distances)-false_positive
-                
+            if threshold < negative_distances[n]:
+                true_pos = i + 1
+                false_negative = len(positive_distances) - true_pos
+                false_positive = n
+                true_negative = len(negative_distances) - false_positive
 
-
-                self.thresholds.append(threshhold)
+                self.thresholds.append(threshold)
                 self.true_positive.append(true_pos)
                 self.false_negative.append(false_negative)
                 self.false_positive.append(false_positive)
                 self.true_negative.append(true_negative)
-                i+=1
+                i += 1
             else:
-                n+=1
-        
-        if n< len(negative_distances):
+                n += 1
+
+        if n < len(negative_distances):
             thresholds = negative_distances[n::10]
             for th in thresholds:
-                while (th>negative_distances[n]):
-                    if n+1 == len(negative_distances):
+                while th > negative_distances[n]:
+                    if n + 1 == len(negative_distances):
                         break
-                    n+=1
+                    n += 1
                 self.thresholds.append(th)
 
-                true_pos=i+1
-                false_negative=len(positive_distances)-true_pos
-                false_positive=n
-                true_negative=len(negative_distances)-false_positive
+                true_pos = i + 1
+                false_negative = len(positive_distances) - true_pos
+                false_positive = n
+                true_negative = len(negative_distances) - false_positive
                 self.true_positive.append(true_pos)
                 self.false_negative.append(false_negative)
                 self.false_positive.append(false_positive)
                 self.true_negative.append(true_negative)
 
-                    
-
-
-            
-
-        
-        
-        #return self.points
+        # return self.points
 
     def calculate_rates(self):
-        self.true_positives_rate = np.divide(self.true_positive , ( np.add(self.true_positive , self.false_negative )))
-        self.false_positives_rate= np.divide(self.false_positive , ( np.add(self.false_positive , self.true_negative )))
-        self.precision = np.divide( self.true_positive , ( np.add(self.true_positive , self.false_positive )))
+        self.true_positives_rate = np.divide(self.true_positive, (np.add(self.true_positive, self.false_negative)))
+        self.false_positives_rate = np.divide(self.false_positive, (np.add(self.false_positive, self.true_negative)))
+        self.precision = np.divide(self.true_positive, (np.add(self.true_positive, self.false_positive)))
         return 1
-    
+
     def calculate_area_under_graph(self):
         area = 0
-        for i in range(len(self.thresholds)-1):
-           area += ( self.true_positives_rate[i] + self.true_positives_rate[i+1] ) / 2 * ( self.false_positives_rate[i+1] - self.false_positives_rate[i] ) 
+        for i in range(len(self.thresholds) - 1):
+            area += (
+                (self.true_positives_rate[i] + self.true_positives_rate[i + 1])
+                / 2
+                * (self.false_positives_rate[i + 1] - self.false_positives_rate[i])
+            )
         return area
 
-    def plot_ROC(self,options):
+    def plot_ROC(self, options):
         match options:
-            case None | 'FPR':
-                x=self.false_positives_rate
-                x_label='False Positive Rate'
-                y=self.true_positives_rate
-                y_label='True Positive Rate'
-            case 'precision':
-                x=self.true_positives_rate
-                x_label='True Positive Rate'
-                y=self.precision
-                y_label='Precision'
+            case None | "FPR":
+                x = self.false_positives_rate
+                x_label = "False Positive Rate"
+                y = self.true_positives_rate
+                y_label = "True Positive Rate"
+            case "precision":
+                x = self.true_positives_rate
+                x_label = "True Positive Rate"
+                y = self.precision
+                y_label = "Precision"
             case _:
-                print('Option used didnt match any implemented option for x axis value, False Positive Rate was used')
-                x=self.false_positives_rate
-                x_label='False Positive Rate'
-                
-        y=self.true_positives_rate
-        z=self.thresholds
-        fig, ax= plt.subplots()
-        sc= ax.scatter(x , y , c=z, cmap='viridis')
-        plt.fill_between(self.false_positives_rate , self.true_positives_rate, color='red',alpha=0.4)
-        
-        plt.xlim(0,1)
-        plt.ylim(0,1)
+                print("Option used didn't match any implemented option for x axis value, False Positive Rate was used")
+                x = self.false_positives_rate
+                x_label = "False Positive Rate"
+
+        y = self.true_positives_rate
+        z = self.thresholds
+        fig, ax = plt.subplots()
+        sc = ax.scatter(x, y, c=z, cmap="viridis")
+        plt.fill_between(self.false_positives_rate, self.true_positives_rate, color="red", alpha=0.4)
+
+        plt.xlim(0, 1)
+        plt.ylim(0, 1)
         plt.title("ROC Curve")
         plt.xlabel(x_label)
         plt.ylabel(y_label)
 
         cursor = mplcursors.cursor(sc, hover=True)
 
-    # Define what happens when hovering over a point
+        # Define what happens when hovering over a point
         @cursor.connect("add")
         def on_add(sel):
-    # Display the value from the threshold array
-            sel.annotation.set(text=f'threshold={self.thresholds[sel.index]:.3f}')
+            # Display the value from the threshold array
+            sel.annotation.set(text=f"threshold={self.thresholds[sel.index]:.3f}")
 
-    #
+        #
         plt.show()
-
-    
-    
-    
--- runROC.py
+++ runROC.py
@@ -1,46 +1,56 @@
+import argparse
+
+import rasterio
 
 import ROC.ROC as ROC
-import rasterio
-import argparse
+
 
 def get_img_from_tiff(path):
     with rasterio.open(path) as src:
-     im=src.read()
+        im = src.read()
     return im
 
-def run_ROC(path_to_distance_img,path_to_pos_mask,path_to_neg_mask,plot_options):
-    distance_img= get_img_from_tiff(path_to_distance_img)
-    true_positive=get_img_from_tiff(path_to_pos_mask)
-    true_negative=get_img_from_tiff(path_to_neg_mask)
-    roc=ROC.ROC()
 
+def run_ROC(path_to_distance_img, path_to_pos_mask, path_to_neg_mask, plot_options):
+    distance_img = get_img_from_tiff(path_to_distance_img)
+    true_positive = get_img_from_tiff(path_to_pos_mask)
+    true_negative = get_img_from_tiff(path_to_neg_mask)
+    roc = ROC.ROC()
 
-    roc.distance_to_points(distance_img,true_positive,true_negative)
+    roc.distance_to_points(distance_img, true_positive, true_negative)
     roc.calculate_rates()
-    area=roc.calculate_area_under_graph()
+    area = roc.calculate_area_under_graph()
     roc.plot_ROC(plot_options)
     print(f"The Area under curve is approximately: {area}")
 
-if __name__=='__main__':
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-            prog='RecieverOperatorCharacteristics',
-            description='A tool for calculating Reciever Operator Characteristics for colorbased image segmentation.' 
-                        'Based on distance image, positive mask and negative mask.',
-            epilog='Program written by Søren Vad Iversen(soiv@mmmi.sdu.dk) in '
-                    '2024 as part of the Precisionseedbreeding project supported '
-                    'by GUDP and Frøafgiftsfonden.')
-    parser.add_argument('path_to_distance_image',
-                    help='The path for the distance image(.tif) for which the positive and negative mask has been made')
-    parser.add_argument('path_to_positive_mask',
-                    help='The path to the file(.tif) containing the a mask of the indicating places in the image where where the segmentation should characterise as positive, with value 255 indicating positive.')
-    parser.add_argument('path_to_negative_mask',
-                    help='The path to the file(.tif) containing the a mask of the indicating places in the image where where the segmentation should NOT characterise as positive, with value 255 indicating negative.')
-    parser.add_argument('--plot_options',
-                    default=None,
-                    help='Plotting options default is False Positive Rate alternative plotting options are precision')
-    
+        prog="RecieverOperatorCharacteristics",
+        description="A tool for calculating Receiver Operator Characteristics for colorbased image segmentation."
+        "Based on distance image, positive mask and negative mask.",
+        epilog="Program written by Søren Vad Iversen(soiv@mmmi.sdu.dk) in "
+        "2024 as part of the Precisionseedbreeding project supported "
+        "by GUDP and Frøafgiftsfonden.",
+    )
+    parser.add_argument(
+        "path_to_distance_image",
+        help="The path for the distance image(.tif) for which the positive and negative mask has been made",
+    )
+    parser.add_argument(
+        "path_to_positive_mask",
+        help="The path to the file(.tif) containing the a mask of the indicating places in the image where where the segmentation should characterise as positive, with value 255 indicating positive.",
+    )
+    parser.add_argument(
+        "path_to_negative_mask",
+        help="The path to the file(.tif) containing the a mask of the indicating places in the image where where the segmentation should NOT characterise as positive, with value 255 indicating negative.",
+    )
+    parser.add_argument(
+        "--plot_options",
+        default=None,
+        help="Plotting options default is False Positive Rate alternative plotting options are precision",
+    )
 
-    args=parser.parse_args()
+    args = parser.parse_args()
 
-
-    run_ROC(args.path_to_distance_image,args.path_to_positive_mask,args.path_to_negative_mask,args.plot_options)
+    run_ROC(args.path_to_distance_image, args.path_to_positive_mask, args.path_to_negative_mask, args.plot_options)
