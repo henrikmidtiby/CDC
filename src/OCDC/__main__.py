74e4b231bba9f405c3f3050ce3fc4b8ccf2af0fa
Henrik Dyrberg Egemose hde <hde@mmmi.sdu.dk>
2025-01-30 13:47:35 +0100

    Added groups to arg parser

--- src/OCDC/__main__.py
+++ src/OCDC/__main__.py
@@ -27,107 +27,113 @@ def _get_parser() -> argparse.ArgumentParser:
                    2023-{datetime.now().year} as part of the Precisionseedbreeding project
                    supported by GUDP and Frøafgiftsfonden.""",
     )
-    parser.add_argument("orthomosaic", help="Path to the orthomosaic that you want to process.", type=pathlib.Path)
-    parser.add_argument("reference", help="Path to the reference image.", type=pathlib.Path)
-    parser.add_argument("annotated", help="Path to the annotated reference image.", type=pathlib.Path)
-    parser.add_argument(
-        "--bands_to_use",
-        default=None,
-        type=int,
-        nargs="+",
-        help="The bands needed to be analyzed, written as a list, 0 indexed. If no value is specified all bands except alpha channel will be analyzed.",
-    )
-    parser.add_argument(
-        "--alpha_channel",
-        default=-1,
-        type=int,
-        help="Alpha channel number 0 indexed. If no value is specified the last channel is assumed to be the alpha channel. If the orthomosaic does not contain an alpha channel use None.",
-    )
-    parser.add_argument(
-        "--scale",
-        default=5,
-        type=float,
-        help="The calculated distances are multiplied with this factor before the result is saved as an image. Default value is 5.",
-    )
-    parser.add_argument(
+    input_group = parser.add_argument_group("Input")
+    input_group.add_argument("orthomosaic", help="Path to the orthomosaic that you want to process.", type=pathlib.Path)
+    input_group.add_argument("reference", help="Path to the reference image.", type=pathlib.Path)
+    input_group.add_argument("annotated", help="Path to the annotated reference image.", type=pathlib.Path)
+    # input_group.add_argument(
+    #     "--ref_pixels_from_csv",
+    #     type=pathlib.Path,
+    #     default=None,
+    #     metavar="CSV",
+    #     help="Load reference pixel from csv file instead of from reference and annotated image.",
+    # )
+    output_group = parser.add_argument_group("Output")
+    output_group.add_argument(
         "--output_location",
         default="output",
         help="The location in which to save the tiles and orthomosaic.",
         type=pathlib.Path,
     )
-    parser.add_argument(
+    output_group.add_argument(
         "--do_not_save_orthomosaic",
         action="store_false",
         help="If set the no orthomosaic of the result is saved at output_location/orthomosaic.tiff. Default is to save orthomosaic.",
     )
-    parser.add_argument(
+    output_group.add_argument(
         "--save_tiles",
         action="store_true",
         help="If set tiles are saved at output_location/tiles. Useful for debugging or parameter tweaking. Default no tiles are saved.",
     )
-    parser.add_argument(
+    output_group.add_argument(
+        "--save_ref_pixels",
+        action="store_true",
+        help="Save the raw, transformed and selected reference pixels in output_location/pixel_values. Default do not save.",
+    )
+    output_group.add_argument(
+        "--save_statistics",
+        action="store_true",
+        help="Save statistics of the processed orthomosaic including a histogram of color distances. Files are saved in output_location/statistics. Default do not save.",
+    )
+    color_model_group = parser.add_argument_group("Color Model")
+    color_model_group.add_argument(
         "--method",
         default="mahalanobis",
         type=str,
         choices=["mahalanobis", "gmm"],
         help="The method used for calculating distances from the set of annotated pixels. Possible values are 'mahalanobis' for using the Mahalanobis distance and 'gmm' for using a Gaussian Mixture Model. 'mahalanobis' is the default value.",
     )
-    parser.add_argument(
+    color_model_group.add_argument(
         "--param",
         default=2,
         type=int,
         help="Numerical parameter for the color model. When using the 'gmm' method, this equals the number of components in the Gaussian Mixture Model.",
     )
-    parser.add_argument(
-        "--tile_size",
-        default=3000,
-        type=int,
-        help="The height and width of tiles that are analyzed. " "Default is 3000.",
+    color_model_group.add_argument(
+        "--scale",
+        default=5,
+        type=float,
+        help="The calculated distances are multiplied with this factor before the result is saved as an image. Default value is 5.",
     )
-    parser.add_argument(
-        "--run_specific_tile",
-        nargs="+",
-        type=int,
-        metavar="TILE_ID",
-        help="If set, only run the specific tile numbers. (--run_specific_tile 16 65) will run tile 16 and 65.",
-    )
-    parser.add_argument(
-        "--run_specific_tileset",
-        nargs="+",
-        type=int,
-        metavar="FROM_TILE_ID TO_TILE_ID",
-        help="takes two inputs like (--from_specific_tileset 16 65). This will run every tile from 16 to 65.",
-    )
-    parser.add_argument(
+    transform_group = parser.add_argument_group("Transforms")
+    transform_group.add_argument(
         "--gamma_transform",
         type=float,
         default=None,
         metavar="GAMMA",
         help="Apply a gamma transform with the given gamma to all inputs. Default no transform.",
     )
-    parser.add_argument(
+    transform_group.add_argument(
         "--lambda_transform",
         type=str,
         default=None,
         metavar="LAMBDA",
         help="Apply a Lambda transform with the given Lambda expression to all inputs. Numpy is available as np. Default no transform.",
     )
-    parser.add_argument(
-        "--save_ref_pixels",
-        action="store_true",
-        help="Save the raw, transformed and selected reference pixels in output_location/pixel_values. Default do not save.",
-    )
-    parser.add_argument(
-        "--save_statistics",
-        action="store_true",
-        help="Save statistics of the processed orthomosaic including a histogram of color distances. Files are saved in output_location/statistics. Default do not save.",
-    )
-    parser.add_argument(
-        "--ref_pixels_from_csv",
-        type=pathlib.Path,
+    image_channels_group = parser.add_argument_group("Image Channels")
+    image_channels_group.add_argument(
+        "--bands_to_use",
         default=None,
-        metavar="CSV",
-        help="Load reference pixel from csv file instead of from reference and annotated image.",
+        type=int,
+        nargs="+",
+        help="The bands needed to be analyzed, written as a list, 0 indexed. If no value is specified all bands except alpha channel will be analyzed.",
+    )
+    image_channels_group.add_argument(
+        "--alpha_channel",
+        default=-1,
+        type=int,
+        help="Alpha channel number 0 indexed. If no value is specified the last channel is assumed to be the alpha channel. If the orthomosaic does not contain an alpha channel use None.",
+    )
+    tile_group = parser.add_argument_group("Tiles")
+    tile_group.add_argument(
+        "--tile_size",
+        default=3000,
+        type=int,
+        help="The height and width of tiles that are analyzed. " "Default is 3000.",
+    )
+    tile_group.add_argument(
+        "--run_specific_tile",
+        nargs="+",
+        type=int,
+        metavar="TILE_ID",
+        help="If set, only run the specific tile numbers. (--run_specific_tile 16 65) will run tile 16 and 65.",
+    )
+    tile_group.add_argument(
+        "--run_specific_tileset",
+        nargs="+",
+        type=int,
+        metavar="FROM_TILE_ID TO_TILE_ID",
+        help="takes two inputs like (--from_specific_tileset 16 65). This will run every tile from 16 to 65.",
     )
     return parser
 
@@ -165,7 +171,6 @@ def _process_color_model_args(args: Any, keyword_args: dict[str, Any]) -> BaseDi
 
 def _main() -> None:
     args = _parse_args()
-    print(args)
     keyword_args = vars(args)
     keyword_args.update(_process_transform_args(args))
     _create_output_location(args.output_location)
