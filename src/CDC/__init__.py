3a4233892e94ca3cab9d25e28c6bc7513f87be40
Henrik Dyrberg Egemose hde <hde@mmmi.sdu.dk>
2025-04-10 11:27:46 +0200

    fix: Handle warnings as errors

--- src/CDC/__init__.py
+++ src/CDC/__init__.py
@@ -1,10 +1,16 @@
 from __future__ import annotations
 
-# Current version
-__version__ = "1.1.0"
+import warnings
 
-# import CDC objects
+from rasterio.errors import NotGeoreferencedWarning
+
 from .color_models import *
 from .orthomosaic_tiler import *
 from .tiled_color_based_distance import *
 from .transforms import *
+
+warnings.filterwarnings("error")
+warnings.filterwarnings("ignore", category=NotGeoreferencedWarning)
+
+# Current version
+__version__ = "1.1.0"
